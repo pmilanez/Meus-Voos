0432554d0d84174b7eaf15a5a17c4915f8814d25
Pedro Milanez <pmilanez@gmail.com>
2012-04-11 17:45:02 -0300

    Validating ICAO code for Airport

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -4,5 +4,13 @@ class Airport < ActiveRecord::Base
   validates_presence_of :longitude, :on => :create, :message => "can't be blank"
   validates_presence_of :icaocode, :on => :create, :message => "can't be blank"
   
+  # Validating the ICAO format
   validates_length_of :icaocode, :within => 4..4, :on => :create, :message => "ICAO CODE must be with 4 digits"
+  validates :icaocode, :format => { :with => /\A[a-zA-Z]+\z/, :message => "Only letters allowed" }
+  before_save :transform_to_upcase_icao_code
+  
+  def transform_to_upcase_icao_code
+    self.icaocode = icaocode.to_s.upcase
+  end
+  
 end
