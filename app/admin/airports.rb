84bc7517fa2ae8597a657db6dd0037898896e2e4
Pedro Milanez <pmilanez@gmail.com>
2012-04-11 18:36:31 -0300

    Customizing Airport admin

--- app/admin/airports.rb
+++ app/admin/airports.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Airport do
+  config.clear_sidebar_sections! 
   
+  index do
+    column :name
+    column "ICAO Code", :icaocode
+    default_actions
+  end
+  
+  form do |f|
+      f.inputs "Airport Information" do
+        f.input :name, :label => "Aiport Name"
+        f.input :latitude, :label => "Latitude (decimal format)"
+        f.input :longitude, :label => "Longitude (decimal format)"
+        f.input :icaocode, :label => "ICAO Code"
+      end
+      f.buttons
+    end
+    
 end
